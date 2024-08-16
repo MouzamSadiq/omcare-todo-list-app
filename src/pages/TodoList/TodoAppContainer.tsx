4be3f0a12c760a2ff3bd823fe6c7d84cf292dc15
MouzamSadiq <mouzam@systalent.com>
2024-08-16 14:16:39 +0530

    feat: Add TodoAppContainer component for managing tasks

---
+++ src/pages/TodoList/TodoAppContainer.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { Box, Container, Typography } from "@mui/material";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+import { generateUniqueId } from "../../utils/helper";
+import { Task } from "../../types";
+
+const TodoAppContainer = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const addTask = (task: string) => {
+    const newTask: Task = {
+      id: generateUniqueId(),
+      text: task,
+    };
+    setTasks([...tasks, newTask]);
+  };
+
+  const deleteTask = (taskToDelete: Task) => {
+    setTasks(tasks.filter((task) => task.id !== taskToDelete.id));
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Box
+        sx={{
+          border: "1px solid",
+          borderRadius: "8px",
+          boxShadow: 3,
+          bgcolor: "#1B1A41",
+          p: 2,
+        }}
+      >
+        <Typography variant="h4" gutterBottom>
+          Todo List
+        </Typography>
+        <TodoForm addTask={addTask} />
+        {tasks.length ? (
+          <TodoList tasks={tasks} deleteTask={deleteTask} />
+        ) : null}
+      </Box>
+    </Container>
+  );
+};
+
+export default TodoAppContainer;
